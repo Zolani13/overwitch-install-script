bd6474826950fdce00fdfd5f68f02f74d38c3c14
Zolani Stewart <zorym26@gmail.com>
2025-07-20 20:42:47 -0400

    Improved script and added readme

---
+++ README.md
@@ -0,0 +1,7 @@
+# Overwitch Install Script
+
+This is a simple Bash script for an easier install of dagargo's JACK Client for Elektron devices onto Ubuntu systems. **This does not install the systemd service option.**
+
+## How to Use
+- Ensure you have git installed onto your computer
+- Enable the ability to execute the script as a program in your File Manager.
--- overwitch-install-script.sh
+++ overwitch-install-script.sh
@@ -1,18 +1,55 @@
 #!/bin/bash
 
+if ! command -v git &> /dev/null; then
+  echo "Error: git is not installed. Please install git and related tools and rerun this script."
+  exit 1
+fi
+
 read -p "Please enter your username to be added to the audio group: " username
 
-echo "Thanks, $username!"
+if id "$username" &>/dev/null; then
 
+echo "Thanks, $username! Adding you to the audio group..."
+
+sudo usermod -aG audio $username
+
+else
+echo "This username does not exist in records. You will have to add yourself to the usergroup manually."
+fi
+
+echo "Downloading repository..."
+
+if [[ ! -d "overwitch" ]]; then
+
+cd "$HOME"
 git clone https://github.com/dagargo/overwitch.git
+
+else 
+    read -p $'Overwitch directory already found. \n[Y] to delete existing folder and re-download into directory \n[N] to exit script  \n[S] to skip download and continue installation' ans
+
+    if [[ "$ans" == "Y" || "$ans" == "y" ]]; then
+    cd "$HOME"
+    sudo rm -rf ./overwitch
+    git clone https://github.com/dagargo/overwitch.git
+
+    elif [[ "$ans" == "N" || "$ans" == "n" ]]; then
+    echo "Exiting..."
+    exit 1
+
+    elif [[ "$ans" == "S" || "$ans" == "s" ]]; then
+    echo "Skipping Download, continuing..."
+
+    else 
+        echo "Invalid Option. Exiting..."
+        exit 1
+    fi
+fi
+
+
 
 echo "Installing build files..."
 
 sudo apt install -y autoconf
-
-echo "Adding you to the audio group..."
-
-sudo usermod -aG audio $username
 
 echo "Installing package dependencies..."
 
